c32b25c52cd8cc65ba07cb72939e8cf9ec9b3d81
aia02 <ymdarboleda22@src.pshs.edu.ph>
2025-02-12 21:37:36 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
  function exponentialPopulationGrowth(){
 	   
-     let initialPopulation = parseFloat(prompt('Enter the initial population :'));
+     let inPopulation = parseFloat(prompt('Enter the initial population :'));
      let rateGrowth = parseFloat(prompt('Enter the rate of growth: '));
-	 let time = parseFloat(prompt('Enter the time in hours: '));
+     let time = parseFloat(prompt('Enter the time in hours: '));
 	               
-     let finalPopulation = Math.round(initialPopulation*Math.pow(Math.E, (rateGrowth*time)));
+     let finPopulation = Math.round(inPopulation*Math.pow(Math.E, (rateGrowth*time)));
                       
-     let monsterLocation = prompt('Enter the region or location of the monster: ');
+     let monsterLoc = prompt('Enter the region or location of the monster: ');
      let monsterName = prompt('Enter the name of the monster: ');
 	 
-	 let nameAndLoc = monsterLocation.concat(" ", monsterName).toUpperCase();
+	 let nameAndLoc = monsterLoc.concat(" ", monsterName).toUpperCase();
                       
-      document.getElementById("result").innerHTML = "After " + time + " hours, the population of " + nameAndLoc + " has increased to " + finalPopulation;
+      document.getElementById("result").innerHTML = "After " + time + " hours, the population of " + nameAndLoc + " has increased to " + finPopulation;
 
  }
